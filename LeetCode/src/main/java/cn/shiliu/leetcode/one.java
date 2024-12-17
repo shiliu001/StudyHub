8085f481a59dbf35fda18a136e4a91c2671bf76a
shiliu <319227835@qq.com>
2024-12-17 23:40:40 +0800

    测试gitee是否可以同步github的仓库提交

---
+++ LeetCode/src/main/java/cn/shiliu/leetcode/one.java
@@ -0,0 +1,12 @@
+package cn.shiliu.leetcode;
+
+/**
+* @author 十六
+* @date 2024/12/17 23:38
+* @description: 算法题第一道
+*/
+public class one {
+    public static void main(String[] args) {
+        System.out.println("one.main");
+    }
+}
